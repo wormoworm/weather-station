3012cb5951bdaaa06988c3dafe424f75b5a65053
Tom Homewood <tomhomewood@gmail.com>
2021-04-27 00:00:39 +0100

    Using SPI for comms with BME280, as I2C was noncing me about.

--- pi/python/weather-station.py
+++ pi/python/weather-station.py
@@ -1,6 +1,8 @@
 import json
 import time
-from bme280pi import Sensor
+import board
+import digitalio
+import adafruit_bme280
 from time import sleep
 from paho_iot_client import PahoIoTClient, IotClientConfig
 
@@ -9,8 +11,12 @@ SAMPLING_INTERVAL_S = 30
 def get_current_time_s():
     return int(round(time.time()))
 
-# Initialise the BME280 sensor, using I2C address 0x77
-sensor = Sensor(address=0x77)
+# NOTE: Pin numbers correspond to the GPIO_xx numbers on pinout.xyz
+
+# Create sensor object, using the board's default SPI bus.
+spi = board.SPI()
+bme_cs = digitalio.DigitalInOut(board.D5)
+bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
 
 paho_config = IotClientConfig("/home/pi/paho/paho_config.json")
 paho_client = PahoIoTClient(config = paho_config, event_listener = None)
@@ -20,9 +26,9 @@ paho_client.connect()
 # Sample the sensor once per second in a loop
 while True:
     data = {
-        "temperature": round(sensor.get_temperature(), 1),
-        "pressure": round(sensor.get_pressure(), 3),
-        "humidity": round(sensor.get_humidity(), 1)
+        "temperature": round(bme280.temperature, 1),
+        "pressure": round(bme280.pressure, 3),
+        "humidity": round(bme280.relative_humidity, 1)
     }
     message = {
         "timestamp": get_current_time_s(),
